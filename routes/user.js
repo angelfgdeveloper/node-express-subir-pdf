bac4a9335e8ef6222bdad1297fcd4ff0b613bd62
Luis Angel <angelfgdeveloper@gmail.com>
2022-02-25 13:01:56 -0600

    Correccion de nombres en validators

--- routes/user.js
+++ routes/user.js
@@ -8,18 +8,18 @@ const router = Router();
 
 router.post('/personal', [
   validJWT,
-  check('motherLastName', 'El motherLastName es obligatorio').not().isEmpty(),
-  check('lastName', 'El lastName es obligatorio').not().isEmpty(),
-  check('name', 'El name es obligatorio').not().isEmpty(),
-  check('dateOfBirth', 'El dateOfBirth es obligatorio').not().isEmpty(),
-  check('gender', 'El gender es obligatorio').not().isEmpty(),
-  check('state', 'El state es obligatorio').not().isEmpty(),
-  check('town', 'El town es obligatorio').not().isEmpty(),
-  check('suburb', 'El suburb es obligatorio').not().isEmpty(),
-  check('street', 'El street es obligatorio').not().isEmpty(),
-  check('numberHome', 'El numberHome es obligatorio').not().isEmpty(),
-  check('postalCode', 'El postalCode es obligatorio').not().isEmpty(),
-  check('cellphone', 'El cellphone es obligatorio').not().isEmpty(),
+  check('motherLastName', 'El apellido materno es obligatorio').not().isEmpty(),
+  check('lastName', 'El apellido paterno es obligatorio').not().isEmpty(),
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('dateOfBirth', 'La fecha de nacimiento es obligatoria').not().isEmpty(),
+  check('gender', 'El genero es obligatorio').not().isEmpty(),
+  check('state', 'El estado es obligatorio').not().isEmpty(),
+  check('town', 'El municipio es obligatorio').not().isEmpty(),
+  check('suburb', 'La colonia o fracc. es obligatorio').not().isEmpty(),
+  check('street', 'La calle es obligatoria').not().isEmpty(),
+  check('numberHome', 'El número interior/exterior es obligatorio').not().isEmpty(),
+  check('postalCode', 'El codigo postal es obligatorio').not().isEmpty(),
+  check('cellphone', 'El numero celular es obligatorio').not().isEmpty(),
   validfields
 ], personalPost);
 
